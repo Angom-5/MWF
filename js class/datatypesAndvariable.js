5f102311f6795629c13e61fa8a5c6b101f2ac579
Angom-5 <cangom@student.refactory.academy>
2025-07-29 10:00:10 +0300

    adding js class file

---
+++ js class/datatypesAndvariable.js
@@ -0,0 +1,36 @@
+/*data tyes
+>primitive data tyes= hold single values
+strings is text in js- in ""
+
+numbers are integers, big integer and decimals- written lain
+
+boolean true/false
+
+undefined/=0; nul. Dn't use 0 cause its a value. just put nul
+
+symbols; uniqueness
+>Non-primitive- take in multi values
+1. array-list of items
+[use this]
+[numbers]
+[1,2,3,4]
+[text]
+["bed","beat", "red"]
+let pdtList be=["bed",..]
+
+2. object
+everything i want is an object
+if the thing has many descriptions
+let variableObject = {
+name:"bed",
+price:300k,
+}
+3. date
+4. function 
+5.Know the declaration, variable and the datatpye
+let area="kira"
+let-declaration
+area-variable
+kira-datatype(string)
+*/
+//if something is a constant:use const variable=
